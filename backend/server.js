88fef1d28b74db3e379194600fcc76fc75a844d2
KunalNasa <kannunasa@gmail.com>
2024-10-16 03:00:44 +0530

    Till send messages controller

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/auth.routes.js"
-
+import connectDb from "./db/connectDB.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectDb();
 
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
